e3d383ca8506e04d8811722524d13010cc09963a
Floriangomis <florian.gomis@viacesi.fr>
2018-03-28 08:18:49 +0100

    :art: remove useless bind(this) and remove for attribute on label

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -23,15 +23,15 @@ class LoginForm extends React.Component {
             <React.Fragment>
                 <div className="login-container">
                     <div className="login-input">
-                        <label for="login"> Login </label>
+                        <label> Login </label>
                         <input type="text" ref={this.loginRef} name="login" />
                     </div>
                     <div className="password-input">
-                        <label for="password"> Password </label>
+                        <label> Password </label>
                         <input type="password" ref={this.passwordRef} name="password" />
                     </div>
                     <div className="login-btn">
-                        <button onClick={this.sendMessage.bind(this)}> Log in </button>
+                        <button onClick={this.sendMessage}> Log in </button>
                     </div>
                 </div>
             </React.Fragment>
